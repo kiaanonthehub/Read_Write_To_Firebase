c0d82b46460ed9397d09d388cda50764eab94dc4
Kiaan Maharaj <kiaan.maahraj@outlook.com>
2022-05-17 18:51:10 +0200

    implemented DbHandler class

---
+++ app/src/main/java/com/kiaan/readwritetofirebase/DbHandler.java
@@ -0,0 +1,13 @@
+package com.kiaan.readwritetofirebase;
+
+public class DbHandler {
+
+    // method to write object to real time database
+    public void writeToFirebase() {
+    }
+
+    // method to read json data from real time database to object class
+    public void readFromFirebase() {
+    }
+
+}
