6f097c6535e0a8f0d548081e4f2fe866fb53f695
Kiaan Maharaj <kiaan.maahraj@outlook.com>
2022-05-17 19:20:11 +0200

    renamed UI components id

--- app/src/main/java/com/kiaan/readwritetofirebase/DbHandler.java
+++ app/src/main/java/com/kiaan/readwritetofirebase/DbHandler.java
@@ -1,13 +1,26 @@
 package com.kiaan.readwritetofirebase;
 
-public class DbHandler {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public  class DbHandler {
+
+    private DatabaseReference mDatabase;
 
     // method to write object to real time database
-    public void writeToFirebase() {
+    public void writeToFirebase(String tableName, String uID, Object obj) {
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child(tableName).child(uID).setValue(obj);
     }
 
-    // method to read json data from real time database to object class
-    public void readFromFirebase() {
+    //overload method to write nested child object to real time database
+    public void writeToFirebase(String tableName, String uID, String tableName2, String uID2, Object obj) {
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child(tableName).child(uID).child(tableName2).child(uID2).setValue(obj);
     }
+
+
 
 }
